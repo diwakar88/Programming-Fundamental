35698af6222cfaaa75deac691c8e19d8c79f0693
diwakar88 <damodarkandel435@gmail.com>
2016-09-18 11:52:42 +0545

    Add files via upload
    Area of

---
+++ SQUARE.C
@@ -0,0 +1,21 @@
+/*
+Student Name:Diwakar kandel
+Subject:Programming Fund
+Roll No:
+Program:to print square
+Lab No:01
+Date:2016-09-17
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main(){
+
+float a,result;
+clrscr();
+printf("Enter any no:");
+scanf("%f",&a);
+result=sqrt(a);
+printf("square of %f",result);
+getch();
+}
